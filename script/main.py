1b00dba997e232e76cfc1d794d9d7fe5f91a199c
repen <9keepa@gmail.com>
2021-01-10 22:55:53 +0500

    проект создан

---
+++ script/main.py
@@ -0,0 +1,9 @@
+"""
+Copyright 2021 Andrey Plugin (9keepa@gmail.com)
+Licensed under the Apache License v2.0
+http://www.apache.org/licenses/LICENSE-2.0
+"""
+from tool import log as _log
+log = _log("MAIN")
+
+log.info("This logger for main script")
