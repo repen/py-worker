51b05f8b32f4a051604928f5058e3703874218a6
repen <9keepa@gmail.com>
2022-02-13 17:17:16 +0500

    del folder log and refactoring project

--- script/database.py
+++ script/database.py
@@ -1,3 +1,8 @@
+"""
+Copyright 2022 Andrey Plugin (9keepa@gmail.com)
+Licensed under the Apache License v2.0
+http://www.apache.org/licenses/LICENSE-2.0
+"""
 from config import BaseEnvironment, ProductionEnvironment, \
     DevelopmentEnvironment, TestingEnvironment
 from typing import Union
--- script/tool.py
+++ script/tool.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 Andrey Plugin (9keepa@gmail.com)
+Copyright 2022 Andrey Plugin (9keepa@gmail.com)
 Licensed under the Apache License v2.0
 http://www.apache.org/licenses/LICENSE-2.0
 """
